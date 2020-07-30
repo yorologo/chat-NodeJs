81fdb6bb877505d22d8562522a0a60369b06cb88
yorologo <esaud@hotmail.es>
2020-07-30 14:43:31 -0500

    Probando las peticiones

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 
-let port = 3000;
 let app = express();
+let port = 3000;
 
-app.use("/", (request, response) => {
-  response.send("Hola");
+// app.use("/", (request, response) => {
+//   response.send("Hola");
+// });
+
+// GET method route
+app.get("/message", function (req, res) {
+  res.send("Lista de mensajes");
+});
+
+// POST method route
+app.post("/message", function (req, res) {
+  res.send("Mensaje añadido correctamente");
+});
+
+// DELETE method route
+app.delete("/message", function (req, res) {
+  res.send("Mensaje borrado correctamente");
 });
 
 app.listen(port);
