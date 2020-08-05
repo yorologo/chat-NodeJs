901f81f32455afbd3a9a59527b1e9b98664e9734
yorologo <esaud@hotmail.es>
2020-08-05 12:17:10 -0500

    Gestion de conexiones a la DB desde la API

--- components/message/store.js
+++ components/message/store.js
@@ -1,15 +1,4 @@
-const db = require("mongoose");
 const Model = require("./model");
-
-db.Promise = global.Promise;
-db.connect(
-  "mongodb+srv://db_newUser:6MxEPqi3aQQnjjCM@cluster0.yebum.mongodb.net/CursoNodeJS?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-console.log("[database] Conexion realizada correctamente");
 
 function addMessage(message) {
   const myMessage = new Model(message);
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const router = require("./components/message/network");
+const db = require("./db");
 const router = require("./network/routes");
+
+db(
+  "mongodb+srv://db_newUser:6MxEPqi3aQQnjjCM@cluster0.yebum.mongodb.net/CursoNodeJS?retryWrites=true&w=majority"
+);
 
 let port = 3000;
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(router);
 router(app);
 
 app.use("/app", express.static("public"));
